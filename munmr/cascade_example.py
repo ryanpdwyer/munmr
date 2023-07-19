5e6f0f0131e145d1da2d0bb31f070d0f9c057a19
Ryan Dwyer <ryanpdwyer@gmail.com>
2023-07-19 08:47:41 -0400

    Refactored cascade example to do the pickling, saving, loading all together in one function.

--- munmr/cascade_example.py
+++ munmr/cascade_example.py
@@ -8,22 +8,29 @@ import pickle
 
 
 @st.experimental_memo
-def cascade(smiles, fname):
-    return subprocess.run([sys.executable, "run_cascade.py", smiles, fname])
+def cascade(smiles):
+    """Runs cascade using a separate python process, saving the results to a pickle file,
+    then loading them back into the main process."""
+    fname = f"output/{hash(smiles)}.pickle"
+
+    return_code = subprocess.run([sys.executable, "run_cascade.py", smiles, fname])
+    # pickle file saves a dictionary with keys outC and outH, 
+    # the outputs from running the cascade Carbon and Hydrogen
+    # prediction models
+    with open(fname, "rb") as f:
+        out = pickle.load(f)
+
+    return out["outC"], out["outH"]
+
+
 
 # Use streamlit to output the python environment / path
 st.write(sys.executable)
 
 smiles = st.text_input("SMILES", "CCO")
 
-fname = f"output/{hash(smiles)}.pickle"
-
-cascade(smiles, fname)
-
-with open(fname, "rb") as f:
-    out = pickle.load(f)
-
-outC, outH = out["outC"], out["outH"]
+# Run cascade using a separate python process
+outC, outH = cascade(smiles)
 
 st.write(outC[0])
 st.write(outC[1])
